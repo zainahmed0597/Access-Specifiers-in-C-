5c5e950ea5ca2eab939019f347de9b7612867c77
Zain Ahmed <48980361+zainahmed0597@users.noreply.github.com>
2020-02-11 16:39:10 +0500

    First commit

---
+++ Access Specifiers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class myClass {
+  public:
+    string name;
+};
+
+int main() {
+  myClass myObj;
+  myObj.name = "SoloLearn";
+  cout << myObj.name;
+  return 0;
+}
+
+
